2ee5fa112eb0af78090f7d8423d470997a34276b
km Habib <31393442+kmhabib71@users.noreply.github.com>
2023-05-22 00:36:09 +0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,73 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+
 
 export default function Home() {
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+   <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bhara.com - Get the Domain</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f5f5f5;
+      margin: 0;
+      padding: 0;
+    }
+    
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 40px 20px;
+      text-align: center;
+    }
+    
+    h1 {
+      color: #333;
+      font-size: 28px;
+      margin-bottom: 20px;
+    }
+    
+    p {
+      color: #666;
+      font-size: 18px;
+      margin-bottom: 30px;
+    }
+    
+    .email {
+      font-size: 20px;
+      color: #fff;
+      background-color: #007bff;
+      border-radius: 4px;
+      padding: 10px 20px;
+      text-decoration: none;
+      transition: background-color 0.3s ease;
+    }
+    
+    .email:hover {
+      background-color: #0056b3;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+    <h1>Bhara.com - Get the Domain</h1>
+    <p>
+      TO GET THE DOMAIN CONTACT WITH THE EMAIL BELOW:
+    </p>
+    <p>
+      <strong>mail.bhara.com@gmail.com</strong>
+    </p>
+    <p>You will get a reply within 24 hours.</p>
+    <a href="mailto:mail.bhara.com@gmail.com" class="email">Contact Now</a>
+  </div>
+</body>
+
+</html>
+
   );
 }
